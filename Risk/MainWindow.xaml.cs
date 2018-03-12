1b054338edbc9365e00e79b0b21cab03e35896de
Viktor Vašátko <viktor.vasatko@seznam.cz>
2018-03-12 14:45:44 +0100

    Removing close button in right-top corner

--- Risk/MainWindow.xaml.cs
+++ Risk/MainWindow.xaml.cs
@@ -20,9 +20,29 @@ namespace Risk
   /// </summary>
   public partial class MainWindow : Window
   {
+    /// <summary>
+    /// Removing close button in right-top corner of window.
+    /// </summary>
+    private const int GWL_STYLE = -16;
+
+    private const int WS_SYSMENU = 0x80000;
+
+    [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
     public MainWindow()
     {
+      Loaded += WindowLoaded;
       InitializeComponent();
+    }
+
+    private void WindowLoaded(object sender, RoutedEventArgs e)
+    {
+      var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+      SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
     }
   }
 }
