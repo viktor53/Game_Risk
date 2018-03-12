3cec06878f3a4895ea4b9a6aae46ed07f6473d1f
Viktor Vašátko <viktor.vasatko@seznam.cz>
2018-03-13 00:11:29 +0100

    add Server

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Risk.Networking.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,13 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-  class Program
+  internal class Program
   {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
+      RiskServer server = new RiskServer("localhost", 11000);
+
+      server.Start();
     }
   }
 }
